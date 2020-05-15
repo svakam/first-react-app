c8fb58fca21f1beda1c77e19435a6665ed7116cc
svakam <vikramakam@hotmail.com>
2020-05-14 22:01:55 -0700

    stateless functional vs stateful class

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-/* responsible for HTML (view) displayed in browser */
+/* responsible for HTML (view) displayed in browser
+ content changes dynamically - will be changed in browser if updated here
+ this is the App component placed in a .js file
+ this is an example of a stateless functional component: the code is a JS function which returns HTML, which describes the UI */
 function App() {
   return (
     <div className="App">
